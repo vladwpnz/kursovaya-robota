57fb64f1b7670596d469bb18e7689d5fe2064ea8
vladwpnz <90748608+vladwpnz@users.noreply.github.com>
2023-05-24 20:45:17 +0300

    Add files via upload

---
+++ PlatformerFinal/PlatformerFinal/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlatformerFinal
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight, jumping, isGameOver;             
+
+        int jumpSpeed;
+        int force;
+        int score = 0;
+        int playerSpeed = 7;
+
+        int horizontalSpeed = 5;
+        int verticalSpeed = 3;
+
+        int enemyOneSpeed = 5;
+        int enemyTwoSpeed = 3;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void pictureBox19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Рахунок: " + score;
+
+            player.Top += jumpSpeed;
+
+            if (goLeft == true)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true)
+            {
+                player.Left += playerSpeed;
+            }
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -8;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 10;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+
+
+                    if ((string)x.Tag == "platform")                            // перевірка на зіткнення 
+                    {
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            force = 8;
+                            player.Top = x.Top - player.Height;
+
+
+                            if ((string)x.Name == "horizontalPlatform" && goLeft == false || (string)x.Name == "horizontalPlatform" && goRight == false)
+                            {
+                                player.Left -= horizontalSpeed;
+                            }
+
+
+                        }
+
+                        x.BringToFront();
+
+                    }
+
+                    if ((string)x.Tag == "coin")
+                    {
+                        if (player.Bounds.IntersectsWith(x.Bounds) && x.Visible == true)
+                        {
+                            x.Visible = false;
+                            score++;
+                        }
+                    }
+
+
+                    if ((string)x.Tag == "enemy")
+                    {
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            gameTimer.Stop();
+                            isGameOver = true;
+                            txtScore.Text = "Рахунок: " + score + Environment.NewLine + "Ти упав у бездну!";
+                        }
+                    }
+
+                }
+            }
+
+
+            horizontalPlatform.Left -= horizontalSpeed;                                                                         //Я рухаюсь разом за платформою
+
+            if (horizontalPlatform.Left < 0 || horizontalPlatform.Left + horizontalPlatform.Width > this.ClientSize.Width)
+            {
+                horizontalSpeed = -horizontalSpeed;
+            }
+
+            verticalPlatform.Top += verticalSpeed;
+
+            if (verticalPlatform.Top < 195 || verticalPlatform.Top > 581)
+            {
+                verticalSpeed = -verticalSpeed;
+            }
+
+
+            enemyOne.Left -= enemyOneSpeed;
+
+            if (enemyOne.Left < pictureBox5.Left || enemyOne.Left + enemyOne.Width > pictureBox5.Left + pictureBox5.Width)
+            {
+                enemyOneSpeed = -enemyOneSpeed;
+            }
+
+            enemyTwo.Left += enemyTwoSpeed;
+
+            if (enemyTwo.Left < pictureBox2.Left || enemyTwo.Left + enemyTwo.Width > pictureBox2.Left + pictureBox2.Width)
+            {
+                enemyTwoSpeed = -enemyTwoSpeed;
+            }
+
+
+            if (player.Top + player.Height > this.ClientSize.Height + 50)
+            {
+                gameTimer.Stop();
+                isGameOver = true;
+                txtScore.Text = "Рахунок: " + score + Environment.NewLine + "Тебе вбили";
+            }
+
+            if (player.Bounds.IntersectsWith(door.Bounds) && score == 30)
+            {
+                gameTimer.Stop();
+                isGameOver = true;
+                txtScore.Text = "Рахунок: " + score + Environment.NewLine + "Усі моенти зібрано!";
+            }
+            else
+            {
+                txtScore.Text = "Рахунок: " + score + Environment.NewLine + "Збери усі монети!!!";
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+                                                                             //рух вліво, вправо, прижок
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                RestartGame();
+            }
+
+
+        }
+
+
+        private void RestartGame()
+        {
+
+            jumping = false;                                              //Перезапуск гри
+            goLeft = false;
+            goRight = false;
+            isGameOver = false;
+            score = 0;
+
+            txtScore.Text = "Рахунок: " + score;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+
+
+
+                                      //Позиціі платформ, гравця, ворогів після перезапуску
+            player.Left = 72;                    
+            player.Top = 656;
+
+            enemyOne.Left = 471;
+            enemyTwo.Left = 360;
+
+            horizontalPlatform.Left = 275;
+            verticalPlatform.Top = 581;
+
+            gameTimer.Start();
+
+
+        }
+    }
+}
